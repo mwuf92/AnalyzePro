7a221ad9249a984746b355aaa2308ad9e7332d2d
Forhad <mwuf92@gmail.com>
2023-01-08 16:24:53 +0100

    test branch

--- basicCal.py
+++ basicCal.py
@@ -5,6 +5,22 @@ Created on Sat Jan  7 16:52:23 2023
 @author: hp
 """
 
-class BasicCal():
-    def test_print(self):
-        print("It works Basic Cal")
+class BasicCal:
+    def sum_two_nums(self, num1, num2):
+        res = num1 + num2
+        print(res)
+        return res
+    def subtract_two_nums(self, num1, num2):
+        res = num1 - num2
+        print(res)
+        return res
+    def multi_two_nums(self, num1, num2):
+        res = num1 * num2
+        print(res)
+        return res
+    def divide_two_nums(self, num1, num2):
+        if num2 == 0:
+            print("wrong input")
+        res = num1 / num2
+        print(res)
+        return res
--- calculator.py
+++ calculator.py
@@ -11,13 +11,34 @@ from basicCal import BasicCal
 from scientificCal import ScientificCal
 from advancedCal import AdvancedCal
 
+while True:
+    cal_type = input("Which calculator: \n b for basics\n a for advanced\n s for scientific\n q for quite\n")
+    if cal_type == "s":
+        print(cal_type)
+    elif cal_type == "b":
+        print(cal_type)
+    elif cal_type == "a":
+        print(cal_type)
+    elif cal_type == "q":
+        break
+    else:
+        print("Wrong")
 
-        
-basic_cal_obj = BasicCal()
-basic_cal_obj.test_print()
+
+     
+'''basic_cal_obj = BasicCal()
+
+result = basic_cal_obj.sum_two_nums(9, 10)
+
+print("My reesult is: ", result)
+
+result2 = basic_cal_obj.divide_two_nums(8, 2)
+
+print("My reesult is: ", result2)
+
 
 scientific_cal_obj = ScientificCal()
 scientific_cal_obj.test_print()
 
 advanced_cal_obj = AdvancedCal()
-advanced_cal_obj.test_print()
+advanced_cal_obj.test_print()'''
